b7c45e2e0bb8312987ae6fa0c25f1347f83eb421
PedrOliveiraM <pedropucmont@gmail.com>
2024-09-12 11:00:04 -0300

    update

--- contacts/src/main/java/br/com/pedro/contacts/controller/contactsController.java
+++ contacts/src/main/java/br/com/pedro/contacts/controller/contactsController.java
@@ -1,15 +1,24 @@
 package br.com.pedro.contacts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.pedro.contacts.model.ContactsModel;
+import br.com.pedro.contacts.services.ContactsServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contacts")
 public class ContactsController {
 
+    @Autowired
+    ContactsServices contactsServices;
+
     @GetMapping("")
     public String contacts(){
         return "Hello World, here is contacts";
+    }
+
+    @PostMapping("")
+    public ContactsModel createContact(@RequestBody ContactsModel contact) {
+        return contactsServices.create(contact);
     }
 }
