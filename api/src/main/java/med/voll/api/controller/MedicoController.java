01c5b264819a0efe9ec5c033a020424426c167c1
PedrOliveiraM <pedropucmont@gmail.com>
2024-09-24 19:30:59 -0300

    feat: create and finish doctor controller and register

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DoctorDTO;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -19,7 +20,7 @@ public class MedicoController {
 
     @Transactional
     @PostMapping
-    public void registerDoctor(@RequestBody DoctorDTO data) {
+    public void registerDoctor(@RequestBody @Valid DoctorDTO data) {
         repository.save(new Medico(data));
     }
 }
