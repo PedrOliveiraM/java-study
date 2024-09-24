27f2bda0ced8f367fdb82bf4a608f4b58459774a
PedrOliveiraM <pedropucmont@gmail.com>
2024-09-24 19:31:33 -0300

    fix : update address with validation

--- api/src/main/java/med/voll/api/endereco/AddressDTO.java
+++ api/src/main/java/med/voll/api/endereco/AddressDTO.java
@@ -1,10 +1,25 @@
 package med.voll.api.endereco;
 
-public record AddressDTO(String logradouro,
-                         String bairro,
-                         String cidade,
-                         String cep,
-                         String uf,
-                         String complemento,
-                         String numero) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record AddressDTO(
+        @NotBlank
+        String logradouro,
+
+        @NotBlank
+        String bairro,
+
+        @NotBlank
+        String cidade,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String cep,
+
+        @NotBlank
+        String uf,
+
+        String complemento,
+        String numero) {
 }
